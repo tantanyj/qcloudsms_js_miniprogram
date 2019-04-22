3a59f3d0c6372c676a6c8291d2cdf68530a0dcc3
tantanyj <45837065+tantanyj@users.noreply.github.com>
2019-04-22 19:38:00 +0800

    Update index.js

--- QcloudSms/pages/index/index.js
+++ QcloudSms/pages/index/index.js
@@ -1,14 +1,24 @@
 const app = getApp();
+var MCaptcha = require ('../../utils/Mcaptcha.js');
 
 Page({
   data: {
     hidden: true,
     btnValue: '',
     btnDisabled: false,
-    phone: '',
+    PhoneNumber: '',
+    MCaptchaCode:'',
+    MCaptchaClickTimes: 0,
     VerifyCode: ''
   },
   onReady:function(){
+    //绘制图形验证码
+    this.MCaptcha = new MCaptcha({
+      el: 'canvas',
+      width: 80,
+      height: 35,
+      createCodeImg: ""
+    });
   },
   onLoad: function () {
   },
@@ -16,7 +26,7 @@ Page({
   bindPhoneInput(e) {
     var val = e.detail.value;
     this.setData({
-      phone: val
+      PhoneNumber: val
     })
     //强制要求输入11位
     if (val.length == 11) {
@@ -30,6 +40,33 @@ Page({
       })
     }
   },
+  //刷新验证码
+  changeImg() {
+    var that = this;
+    if (that.data.MCaptchaClickTimes < 6) {
+      that.setData({ MCaptchaClickTimes: that.data.MCaptchaClickTimes + 1 });
+      that.MCaptcha.refresh();
+    }
+    else {
+      //重试次数过多，防止恶意突破强制等待3秒
+      /*这里有个BUG，当重复点击速度过快时，mask防点击穿透生效不及时，触发后的第一次点击仍会有次穿透，建议防重试次数不要设置过低
+      **该问题PC端开发者工具无法复现，手机端真机调试可见 
+      */
+      wx.showToast({
+        icon: 'none',
+        title: '(っ °Д °;)っ神码君跟不上',
+        duration: 3000,
+        mask: true
+      })
+      this.setData({ MCaptchaClickTimes: 0 });
+    }
+  },
+  //图形验证码输入
+  bindMCaptchaCodeInput(e) {
+    this.setData({
+      MCaptchaCode: e.detail.value
+    })
+  },
   //短信验证码输入
   bindVerifyCodeInput(e) {
     this.setData({
@@ -38,11 +75,31 @@ Page({
   },
   //获取短信验证码
   getCode(e) {
+    //图形验证码校验
+    var res = this.MCaptcha.validate(this.data.MCaptchaCode);
+    if (this.data.MCaptchaCode == '' || this.data.MCaptchaCode==null){
+      wx.showToast({
+        title: '请输入图形验证码', 
+        icon: 'none',
+        duration: 1500,
+        mask: true       
+      })
+      return;
+    }
+    if (!res){
+      wx.showToast({
+        title: '图形验证码错误',
+        icon: 'none',
+        duration: 1500,
+        mask: true 
+      })
+      return;
+    }
     var that = this;
     wx.cloud.callFunction({
       name: 'SingleSender',
       data: {
-        number: that.data.phone
+        number: that.data.PhoneNumber
       },
       success(res) {
         console.log(res.result.body),
@@ -59,10 +116,11 @@ Page({
   login(e) {
     var that = this;
     //验证码校验
+    if (that.data.PhoneNumber.length == 11 && that.data.VerifyCode.length == 6) {
       wx.cloud.callFunction({
         name: 'CheckCode',
         data: {
-          PhoneNumber: that.data.phone,
+          PhoneNumber: that.data.PhoneNumber,
           VerifyCode: that.data.VerifyCode
         }, success: res => {
           if (res.result.VerifyStauts == 1) {
@@ -71,7 +129,7 @@ Page({
               icon: 'none',
               duration: 1500
             })
-            console.log(that.data.phone + 'check success')
+            console.log(that.data.PhoneNumber + 'check success')
           }
           else{
             wx.showToast({
@@ -79,9 +137,18 @@ Page({
               icon: 'none',
               duration: 1500
             })
-            console.log(that.data.phone + 'check faild')
+            console.log(that.data.PhoneNumber + 'check faild')
           }
         }     
-      })    
+      })
+    }
+	else {
+      wx.showToast({
+        title: '请完整输入11位手机号/6位短信验证码',
+        icon: 'none',
+        duration: 1500
+        })
+	return		
+	  }
   }
 })
